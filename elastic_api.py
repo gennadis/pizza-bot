dca8ea926a7af0e7e61c5023d8e7bd45689f87fa
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-06 13:26:14 +0300

    Create `pizza` products from prefetched data

--- elastic_api.py
+++ elastic_api.py
@@ -5,7 +5,7 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_json_data(url: str) -> dict:
+def get_json_data(url: str) -> list[dict]:
     response = requests.get(url)
     response.raise_for_status()
 
@@ -25,8 +25,43 @@ def get_credential_token(client_id: str, client_secret: str) -> dict:
     return response.json()
 
 
+def create_product(access_token: str, product_details: dict, sku: int) -> dict:
+    headers = {"Authorization": f"Bearer {access_token}"}
+    json_data = {
+        "data": {
+            "type": "product",
+            "name": product_details.get("name"),
+            "slug": str(product_details.get("id")),
+            "sku": str(sku),
+            "description": product_details.get("description"),
+            "manage_stock": False,
+            "price": [
+                {
+                    "amount": product_details.get("price"),
+                    "currency": "RUB",
+                    "includes_tax": True,
+                },
+            ],
+            "status": "live",
+            "commodity_type": "physical",
+        },
+    }
+
+    response = requests.post(
+        "https://api.moltin.com/v2/products", headers=headers, json=json_data
+    )
+    response.raise_for_status()
+
+    return response.json()
+
+
 def main():
     load_dotenv()
+    access_token = get_credential_token(
+        client_id=os.getenv("ELASTIC_CLIENT_ID"),
+        client_secret=os.getenv("ELASTIC_CLIENT_SECRET"),
+    ).get("access_token")
+
     pizza_addresses_data = get_json_data(
         url="https://dvmn.org/filer/canonical/1558904587/128/"
     )
@@ -34,11 +69,13 @@ def main():
         url="https://dvmn.org/filer/canonical/1558904588/129/"
     )
 
-    elastic_token = get_credential_token(
-        client_id=os.getenv("ELASTIC_CLIENT_ID"),
-        client_secret=os.getenv("ELASTIC_CLIENT_SECRET"),
-    )
-    print(elastic_token)
+    for count, pizza in enumerate(iterable=pizza_menus_data, start=1):
+        product = create_product(
+            access_token=access_token,
+            product_details=pizza,
+            sku=count,
+        )
+        print(product)
 
 
 if __name__ == "__main__":
