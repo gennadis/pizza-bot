50af35810e8751506f92a2553e6532e7b254566b
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-06 11:06:57 +0300

    Get `elastic` credential token

--- elastic_api.py
+++ elastic_api.py
@@ -1,6 +1,8 @@
+import os
 from pprint import pprint
 
 import requests
+from dotenv import load_dotenv
 
 
 def get_json_data(url: str) -> dict:
@@ -10,7 +12,21 @@ def get_json_data(url: str) -> dict:
     return response.json()
 
 
+def get_credential_token(client_id: str, client_secret: str) -> dict:
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials",
+    }
+
+    response = requests.post(url="https://api.moltin.com/oauth/access_token", data=data)
+    response.raise_for_status()
+
+    return response.json()
+
+
 def main():
+    load_dotenv()
     pizza_addresses_data = get_json_data(
         url="https://dvmn.org/filer/canonical/1558904587/128/"
     )
@@ -18,8 +34,11 @@ def main():
         url="https://dvmn.org/filer/canonical/1558904588/129/"
     )
 
-    pprint(pizza_addresses_data)
-    pprint(pizza_menus_data)
+    elastic_token = get_credential_token(
+        client_id=os.getenv("ELASTIC_CLIENT_ID"),
+        client_secret=os.getenv("ELASTIC_CLIENT_SECRET"),
+    )
+    print(elastic_token)
 
 
 if __name__ == "__main__":
