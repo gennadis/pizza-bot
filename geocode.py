f06bd42a3329bb2c095a28a3fe48954d2582164d
Gennady Badinov <gennady.badinov@gmail.com>
2022-04-07 14:29:10 +0300

    Get nearest user `pizzeria` by coordinates

--- geocode.py
+++ geocode.py
@@ -1,3 +1,5 @@
+from geopy import distance
+
 import requests
 
 
@@ -17,3 +19,20 @@ def get_coordinates(yandex_token: str, address: str) -> tuple[str, str]:
     longitude, latitude = most_relevant["GeoObject"]["Point"]["pos"].split(" ")
 
     return longitude, latitude
+
+
+def calculate_distsnce(point_a: tuple[str, str], point_b: tuple[str, str]) -> float:
+    return distance.distance(point_a, point_b).km
+
+
+def get_nearest_pizzeria(user_coordinates: tuple[str, str], pizzerias: list[dict]):
+    for pizzeria in pizzerias:
+        pizzeria_coordinates = (
+            pizzeria["longitude"],
+            pizzeria["latitude"],
+        )
+        pizzeria["distance"] = distance.distance(
+            user_coordinates, pizzeria_coordinates
+        ).km
+
+    return min(pizzerias, key=lambda x: x["distance"])
